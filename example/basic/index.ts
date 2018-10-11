82d42edee03d00129996871e806194695a1e11e4
Ian K Smith <smithki707@gmail.com>
2018-10-11 13:19:59 -0600

    Improve console.log statements in basic example

--- example/basic/index.ts
+++ example/basic/index.ts
@@ -1,5 +1,5 @@
 import { AnthologyClient } from '../../src/index';
 
 const anthology = new AnthologyClient<'small' | 'medium' | 'large'>();
-console.log(anthology.breakpoints);
-console.log(anthology.extract('bg', 'red'));
+console.log('Breakpoints:', anthology.breakpoints);
+console.log('AnthologyRule:', anthology.extract('bg', 'red'));
