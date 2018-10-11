de8e69a318b5a47637a1cef5b58a7fb32c7e5557
Ian K Smith <smithki707@gmail.com>
2018-10-11 01:59:48 -0600

    Add javascript helper class

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Anthology_1 = require("./Anthology");
+exports.Anthology = Anthology_1.Anthology;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { Anthology } from './Anthology';
